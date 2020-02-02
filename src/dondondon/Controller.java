f34f3189d229b0d2e494ea6728de561c469b0e69
ltzz <ltzz@users.noreply.github.com>
2020-02-02 21:19:07 +0900

    First commit
    Signed-off-by: ltzz <ltzz@users.noreply.github.com>

---
+++ src/dondondon/Controller.java
@@ -0,0 +1,4 @@
+package dondondon;
+
+public class Controller {
+}
---
+++ src/dondondon/MainWindow.java
@@ -0,0 +1,19 @@
+package dondondon;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow(String title, int windowWidth, int windowHeight){
+        super(title);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setSize(windowWidth, windowHeight);
+        setLocationRelativeTo(null);
+
+        JPanel pane = new JPanel();
+        getContentPane().add(pane);
+
+        setVisible(true);
+
+    }
+}
