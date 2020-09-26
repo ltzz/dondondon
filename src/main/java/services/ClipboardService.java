559660e02123ed16ab00a995abbebb571d2587ef
ltzz <ltzz@users.noreply.github.com>
2020-09-27 01:32:53 +0900

    feat: エクスプローラから画像コピー→投稿フォームにCtrl＋Vでも画像アップロード効くように
    Signed-off-by: ltzz <ltzz@users.noreply.github.com>

--- src/main/java/services/ClipboardService.java
+++ src/main/java/services/ClipboardService.java
@@ -6,20 +6,30 @@ import javafx.scene.input.Clipboard;
 
 import javax.imageio.ImageIO;
 import java.io.File;
+import java.util.Optional;
 
 public class ClipboardService {
     public static File readImage() {
         Clipboard clipboard = Clipboard.getSystemClipboard();
-        if (!clipboard.hasImage()) {
+        if (!clipboard.hasImage() && !clipboard.hasFiles()) {
             return null;
         }
-        try {
-            File file = File.createTempFile("dondondon_upload_tmp", ".png");
-            Image image = clipboard.getImage();
-            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
-            return file;
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        if(clipboard.hasImage()) {
+            try {
+                File file = File.createTempFile("dondondon_upload_tmp", ".png");
+                Image image = clipboard.getImage();
+                ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+                return file;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(clipboard.hasFiles()){
+            System.out.println(clipboard.getFiles());
+            Optional<File> file = clipboard.getFiles().stream().findFirst();
+            return file.orElse(null);
         }
         return null;
     }
